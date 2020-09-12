a48b83d789a2d4dc9ff8a0b3f6da3717faa1cc95
Pawel <pawelrozniecki13@outlook.com>
2020-09-12 16:11:27 +0200

    pushing srgan code

--- src/main.py
+++ src/main.py
@@ -18,29 +18,28 @@ def main():
     model.to(DEVICE)
     model.load_state_dict(torch.load(MODEL_SAVE_PATH,map_location='cpu'))
 
+
     model.eval()
     # image to resize
     # for file in os.listdir('/home/pawel/PycharmProjects/EnhanceIt/src/images/1'):
 
     input_image = Image.open(path)
     input_image = input_image.convert('RGB')
-    input_image = input_image.resize((int(input_image.size[0]*UPSCALE_FACTOR), int(input_image.size[1]*UPSCALE_FACTOR)), Image.BICUBIC)
-    bicubic = input_image.convert('RGB')
-    bicubic.save('bicubic.png')
+    # input_image = input_image.resize((int(input_image.size[0]*UPSCALE_FACTOR), int(input_image.size[1]*UPSCALE_FACTOR)), Image.BICUBIC)
+    # bicubic = input_image.convert('RGB')
+    # bicubic.save('bicubic.png')
 
     input = Variable(transforms.ToTensor()(input_image)).unsqueeze(0)
-
     input = input.to(DEVICE)
 
     with torch.no_grad():
-        torch.cuda.empty_cache()
-        output = model(input).clamp(0.0,1.0)
+        output = model(input)
 
     output = output.cpu()
 
-    out_img =transforms.ToPILImage()(output[0].data.cpu())
+    out_img = transforms.ToPILImage()(output[0].data.cpu())
 
-    out_img.save('output.png')
+    out_img.save('output1.png')
 
     print("saved sucesfully ")
 
--- src/train.py
+++ src/train.py
@@ -1,19 +1,20 @@
 import sys
-sys.path.append('~/EnhanceIt')
-
+sys.path.append('/Users/pingwin/PycharmProjects/EnhanceIt')
 
 import torch
 import torch.nn as nn
 
 from tqdm import tqdm
 import copy
+from torch.autograd import Variable, backward
+
 
 from math import log10
 
 from src.data_manager import *
 from torch.utils.data.dataloader import DataLoader
 
-from src.model import SRCNN, Generator
+from src.model import SRCNN, Generator, Discriminator
 
 from torchvision.datasets import ImageFolder
 
@@ -22,9 +23,15 @@ import torch.optim as optim
 torch.manual_seed(0)
 torch.cuda.manual_seed(0)
 model = Generator(UPSCALE_FACTOR)
+best_weight = copy.deepcopy(model.state_dict())
+model.load_state_dict(best_weight)
+discriminatorNet = Discriminator()
 model.to(DEVICE)
+discriminatorNet.to(DEVICE)
 
-print("MODEL INFORMATION\n", model)
+
+print("Generator INFORMATION\n", model)
+print("Discriminator INFORMATION\n", discriminatorNet)
 print("initiating SRCNN training... ")
 
 # dataset = ImageFolder(DATASET_PATH, transform=transforms.ToTensor())
@@ -41,58 +48,88 @@ test_loader = DataLoader(get_testing_set(TEST_DATAPATH,SIZE, UPSCALE_FACTOR), ba
 
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters())
-
-best_weight = copy.deepcopy(model.state_dict())
+discriminatorOptim = optim.Adam(discriminatorNet.parameters())
 
 model.train()
 
 for epoch in range(EPOCHS):
 
-    epoch_loss = 0.0
+    discriminatorNet.train()
 
+    epoch_loss = 0.0
     best_psnr = 0.0
+    avg_psnr = 0.0
+    best_epoch = 0
 
     for index, data in enumerate(tqdm(data_loader)):
         optimizer.zero_grad()
 
         running_corrects = 0
         batch, target = data
-        print(batch.shape, target.shape)
+        batch_size = batch.size(0)
 
-        batch = batch.to(DEVICE)
-        target = target.to(DEVICE)
+        #-----------------------------------------#
+        #--------------DISCRIMINATOR--------------#
+        #-----------------------------------------#
 
-        preds = model(batch)
+        real_data = Variable(target).to(DEVICE)
+        batch_data = Variable(batch).to(DEVICE)
+        fake_data = model(batch_data)
 
-        loss = criterion(preds, target)
+        discriminatorNet.zero_grad()
+
+        real_output = discriminatorNet(real_data).mean()
+        fake_output = discriminatorNet(fake_data).mean()
+        discriminator_loss = 1 - real_output + fake_output
+        discriminator_loss.backward(retain_graph=True)
+        discriminatorOptim.step()
+
+        # -----------------------------------------#
+        # ---------------GENERATOR-----------------#
+        # -----------------------------------------#
+
+        model.zero_grad()
+        loss = criterion(fake_data, real_data)
         loss.backward()
         optimizer.step()
+
+        # batch = batch.to(DEVICE)
+        # target = target.to(DEVICE)
+        #
+        # preds = model(batch)
+        #
+        # loss = criterion(preds, target)
+        # loss.backward()
+        # optimizer.step()
         epoch_loss += loss.item()
 
     print("EPOCH {} DONE: AVG. Loss: {:.4f}".format(epoch, epoch_loss / len(data_loader)))
-    torch.save(model.state_dict(), MODEL_SAVE_PATH)
+    # torch.save(model.state_dict(), MODEL_SAVE_PATH)
 
     model.eval()
-    avg_psnr = 0.0
 
     with torch.no_grad():
         for i, d in enumerate(tqdm(test_loader, desc="testing progress")):
             test_image, test_label = d[0].to(DEVICE), d[1].to(DEVICE)
 
-            predication = model(test_image).clamp(0.0, 1.0)
+            predication = model(test_image)
             loss = criterion(predication, test_label)
             psnr = 10 * log10(1 / loss.item())
             avg_psnr += psnr
+
         print("===> Avg. PSNR: {:.4f} dB".format(avg_psnr / len(test_loader)))
 
-        if avg_psnr > best_psnr:
+        if avg_psnr/len(test_loader) > best_psnr:
             best_epoch = epoch
             best_psnr = avg_psnr
             best_weight = copy.deepcopy(model.state_dict())
             torch.save(best_weight, MODEL_SAVE_PATH)
+            print("gello")
 
-        # print('best epoch: {}, psnr: {:.2f}'.format(best_epoch, best_psnr/len(test_loader)))
-        torch.save(best_weight, '/Users/pingwin/PycharmProjects/EnhanceIt/src/models/bestSRGAN.pth')
+        print('best epoch: {}, psnr: {:.2f}'.format(best_epoch, best_psnr/len(test_loader)))
+        torch.save(model.state_dict(), MODEL_SAVE_PATH)
+        torch.save(discriminatorNet.state_dict(), '/Users/pingwin/PycharmProjects/EnhanceIt/src/models/discriminatorModel.pth')
+
 
 
 
